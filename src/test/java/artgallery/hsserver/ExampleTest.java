fac6e7843c80bab42cb8b59b6ed3427a78a6780d
Nikita Kulakov <nvkulakov.dev@gmail.com>
2023-10-14 21:54:34 +0300

    draft: test-containers

--- src/test/java/artgallery/hsserver/ExampleTest.java
+++ src/test/java/artgallery/hsserver/ExampleTest.java
@@ -1,17 +1,34 @@
 package artgallery.hsserver;
 
-
 import org.junit.jupiter.api.Test;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+
+import artgallery.hsserver.dto.UserDTO;
+import artgallery.hsserver.exception.RoleDoesNotExistException;
+import artgallery.hsserver.service.AuthService;
 
 @ContextConfiguration
 @SpringBootTest
 public class ExampleTest {
 
-	@Test
-	public void contextLoads() {
-	}
+  @Autowired
+  AuthService authService;
+
+  @Test
+  public void addUser() throws RoleDoesNotExistException {
+    var userDTO = new UserDTO();
+    userDTO.setLogin("testlogin");
+    userDTO.setPassword("testpwd");
+    authService.register(userDTO);
+  }
+
+  @DynamicPropertySource
+  static void configureProperties(DynamicPropertyRegistry registry) {
+    TestConfiguration.setupRegistry(registry);
+  }
 
 }
