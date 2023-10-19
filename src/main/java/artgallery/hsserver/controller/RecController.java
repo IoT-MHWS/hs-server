0736570c68603d919d61cd8f1dbbd3c0ba34a05a
thermvs <mariagewitter@gmail.com>
2023-10-19 13:38:14 +0300

    feat: add extra functionality

---
+++ src/main/java/artgallery/hsserver/controller/RecController.java
@@ -0,0 +1,52 @@
+package artgallery.hsserver.controller;
+
+import artgallery.hsserver.controller.validator.Validator;
+import artgallery.hsserver.dto.*;
+import artgallery.hsserver.model.Style;
+import artgallery.hsserver.service.RecService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/v1/recs")
+@RequiredArgsConstructor
+public class RecController {
+  private final RecService recService;
+
+  @PostMapping("/rec-artists")
+  public ResponseEntity<?> getFilteredArtists(@RequestBody RecDTO req) {
+    RecValidator validator = new RecValidator();
+    validator.validateRec(req);
+    return ControllerExecutor.execute(validator, () -> {
+      return ResponseEntity.ok().body(recService.getFilteredArtists(req));
+    }, "cannot search artists by parameters");
+  }
+
+  @GetMapping("/{artistId}/paintings")
+  public ResponseEntity<?> getPaintingsByArtistId(@PathVariable Long artistId) {
+    RecValidator validator = new RecValidator();
+    return ControllerExecutor.execute(validator, () -> {
+      return ResponseEntity.ok().body(recService.getPaintingsByArtistId(artistId));
+    }, "cannot find paintings by their artists");
+  }
+
+  private static class RecValidator extends Validator {
+
+    public RecValidator validateRec(RecDTO req) {
+      if (req == null ) {
+        this.addViolation("req", "req is null");
+      }
+      if (req.getYearOfBirth() < 0) {
+        this.addViolation("req", "year_of_creation is not correct");
+      }
+      if (Arrays.stream(Style.values()).map(Enum::name).collect(Collectors.toList()).contains(req.getStyle())) {
+        this.addViolation("style", "style in not correct");
+      }
+      return this;
+    }
+  }
+}
---
+++ src/main/java/artgallery/hsserver/service/RecService.java
@@ -0,0 +1,55 @@
+package artgallery.hsserver.service;
+
+import artgallery.hsserver.dto.ArtistDTO;
+import artgallery.hsserver.dto.PaintingDTO;
+import artgallery.hsserver.dto.RecDTO;
+import artgallery.hsserver.exception.ArtistDoesNotExistException;
+import artgallery.hsserver.model.ArtistEntity;
+import artgallery.hsserver.model.GalleryPaintingEntity;
+import artgallery.hsserver.model.PaintingEntity;
+import artgallery.hsserver.repository.ArtistRepository;
+import artgallery.hsserver.repository.PaintingRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+@Service
+public class RecService {
+  private final PaintingRepository paintingRepository;
+  private final ArtistRepository artistRepository;
+
+  public List<ArtistDTO> getFilteredArtists(RecDTO recDto) {
+      List<ArtistEntity> artists = artistRepository.findByStyleAndYearOfBirthLessThanEqual(recDto.getStyle(), recDto.getYearOfBirth());
+    return mapToArtistDtoList(artists);
+  }
+
+  private List<ArtistDTO> mapToArtistDtoList(List<ArtistEntity> artists) {
+    return artists.stream()
+      .map(artist -> new ArtistDTO(artist.getId(), artist.getName(), artist.getYearOfBirth(), artist.getBio(), artist.getStyle()))
+      .collect(Collectors.toList());
+  }
+
+  public List<PaintingDTO> getPaintingsByArtistId(Long artistId) throws ArtistDoesNotExistException {
+    ArtistEntity artistEntity = artistRepository.findById(artistId).orElseThrow(() ->
+      new ArtistDoesNotExistException(artistId));
+    List<PaintingEntity> paintings = paintingRepository.findByArtistEntityId(artistId);
+    return mapToPaintingDtoList(paintings);
+  }
+
+  private List<PaintingDTO> mapToPaintingDtoList(List<PaintingEntity> paintings) {
+    return paintings.stream()
+      .map(this::mapToPaintingDto)
+      .collect(Collectors.toList());
+  }
+
+  private PaintingDTO mapToPaintingDto(PaintingEntity painting) {
+    List<Long> listIds = painting.getGalleryPaintings().stream()
+      .map(GalleryPaintingEntity::getId)
+      .collect(Collectors.toList());
+    return new PaintingDTO( painting.getId(), painting.getName(), painting.getYearOfCreation(),
+      painting.getArtistEntity().getId(), listIds);
+  }
+}
