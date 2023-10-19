035379ae9b19032c8a82487f7ce5b2e56461f28e
thermvs <mariagewitter@gmail.com>
2023-10-19 13:39:45 +0300

    feat: add find by parameters

--- src/main/java/artgallery/hsserver/repository/ArtistRepository.java
+++ src/main/java/artgallery/hsserver/repository/ArtistRepository.java
@@ -1,11 +1,14 @@
 package artgallery.hsserver.repository;
 
 import artgallery.hsserver.model.ArtistEntity;
+import artgallery.hsserver.model.Style;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface ArtistRepository extends JpaRepository<ArtistEntity, Long> {
+  List<ArtistEntity> findByStyleAndYearOfBirthLessThanEqual(Style style, Integer yearOfBirth);
 }
 
 
